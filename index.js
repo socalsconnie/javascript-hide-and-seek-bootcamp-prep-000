190f71f99df5b29722f2dad9399a7efd488ac2f4
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-05-02 03:12:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+  
+}
+
+function increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < ranks.length; i++) {
+    var liNum = parseInt(ranks[i].innerHTML);
+    ranks[i].innerHTML = (liNum + n).toString();
+  }
+}
+
+function deepestChild() {
+  const grandNode = document.querySelector('#grand-node');
+  const firstDiv = grandNode.children[0];
+  const secondDiv = firstDiv.children[0];
+  const thirdDiv = secondDiv.children[0];
+  const fourthDiv = thirdDiv.children[0];
+  return fourthDiv;
+}
+
