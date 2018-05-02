3f75d8665c9b3fa4bcf6246a8485aa01fba619d8
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-05-02 02:58:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+  
+}
+
+function increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < ranks.length; i++) {
+    var liNum = parseInt(ranks[i].innerHTML);
+    ranks[i].innerHTML = (liNum + n).toString();
+  }
+}
+
